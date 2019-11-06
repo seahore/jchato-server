63f8ea6f594391f7d6c081b7a02086aef877be55
谭视怀 Tan Shihuai <tsh20011021@outlook.com>
2019-11-06 15:23:31 +0800

    修正bash脚本

--- build.sh
+++ build.sh
@@ -1,26 +1,23 @@
 root_dir=$(pwd)
 echo $root_dir
 
-function build() {
-    bin=$root_dir/bin
-    src=$root_dir/src
-    out=$root_dir/out
-    echo bin
-    echo src
-    echo out
+bin=$root_dir/bin
+src=$root_dir/src
+out=$root_dir/out
+echo $bin
+echo $src
+echo $out
 
-    touch $root_dir/srclist
-    find $src -name "*.java" > $root_dir/srclist
-    cat $root_dir/srclist
+touch $root_dir/srclist
+find $src -name "*.java" > $root_dir/srclist
+cat $root_dir/srclist
 
-    javac -d $out/classes -encoding utf-8 -g -sourcepath $src @srclist
+mkdir $out/classes
+javac -d $out/classes -encoding utf-8 -g -sourcepath $src @srclist
 
-    cd $out/claases
-    mkdir $out/jar
-    jar -cvfm $out/jar/java-socket-server.jar *
+cd $out/classes
+mkdir $out/jar
+jar -cvfm $out/jar/java-socket-server.jar *
 
-    sudo chmod a+x $out/jar/java-socket-server.jar
-}
-
-build
+sudo chmod 755 $out/jar/java-socket-server.jar
 exit 0
