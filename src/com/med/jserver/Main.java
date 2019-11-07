7ae11d35c8447950672981ddfcab3ae959cf2b48
谭视怀 Tan Shihuai <tsh20011021@outlook.com>
2019-11-07 14:03:45 +0800

    改名

--- src/com/med/jserver/Main.java
+++ src/com/med/jserver/Main.java
@@ -3,7 +3,7 @@ package com.med.jserver;
 import java.io.IOException;
 import java.net.BindException;
 
-public class JChatoServer {
+public class Main {
     public static void main(String[] args) {
         int port;
 
